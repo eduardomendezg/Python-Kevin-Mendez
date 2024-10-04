ae5800dce17fa9f8f283f98578a62313c3a07e2e
eduardomendezg <smss126823@ugb.edu.sv>
2024-10-03 21:08:13 -0600

    clases de graficas

---
+++ Clases Practica/031024-1.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt 
+"""dep = ["San Miguel","Morazán","La unión","Usulutan"]
+frec = [18,2,3,3]
+colores = ["purple", "blue","yellow","black"]
+plt.bar(dep, frec, color = colores)
+plt.title("Lugares de prodecencia")
+plt.xlabel("departamentos")
+plt.ylabel("frecuencia")
+plt.show()
+
+
+cd = ["Taco Bell","Pizza Hut","Wendys","McDonalds","China Wok"]
+frec = [1,15,11,2,5]
+colors = ["#2B303A","#58A4B0","#D64933","#BAC1B8","#0C7C59"]
+plt.bar(cd, frec, color = colors)
+plt.title("Comida de preferencia")
+plt.xlabel("tipos de comida")
+plt.ylabel("frecuencia")
+plt.show() 
+
+
+cd = ["Taco Bell","Pizza Hut","Wendys","McDonalds","China Wok"]
+frec = [1,15,11,2,5]
+colors = ["#2B303A","#58A4B0","#D64933","#BAC1B8","#0C7C59"]
+plt.pie(frec,labels= cd,autopct="%1.1f%%",colors=colors)
+plt.title("Comida de preferencia")
+plt.show() """
---
+++ Clases Practica/031024.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+"""plt.plot([1,2,3,4,5])
+plt.show()"""
+x = [1,2,3,4,5,6]
+y = [10,5,7,8,1,4]
+
+plt.plot(x,y)
+plt.title("Numeros escogidos")
+plt.xlabel("Numeros")
+plt.ylabel("Frecuencia")
+plt.show()
